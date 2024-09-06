b3a57a8a539f840f472e307c1095f22d3089b5fe
jechoi1111 <jechoi1111@gmail.com>
2024-09-06 23:55:00 +0900

    feat: metadata 적용

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'About Us',
+}
+
 export default function AboutUs() {
     return <div><h1>About Us</h1></div>
 }
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <div><h1>Hello NextJS</h1></div>
-}
